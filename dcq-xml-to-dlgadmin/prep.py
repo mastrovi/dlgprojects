168361031e8f2c745973b480d88a4cf5d8650fff
mastrovi <45972311+mastrovi@users.noreply.github.com>
2019-01-17 10:33:41 -0500

    Update prep.py
    Date fix for Atlanta History Center dates (YYYY month D, YYYY month DD, or YYYY-month) and removing extra Repox info from the <record> field

--- dcq-xml-to-dlgadmin/prep.py
+++ dcq-xml-to-dlgadmin/prep.py
@@ -41,11 +41,173 @@ if re.search(r'<dc:identifier>(http.*?)</dc:identifier>', filedata) is not None:
 if re.search(r'<dcterms:isShownAt>http.*\.jpg</dcterms:isShownAt>', filedata) is not None:
     filedata = re.sub(r'<dcterms:isShownAt>http.*\.jpg</dcterms:isShownAt>', "", filedata)
     print ('Removing dcterms:isShownAt ending with .jpg')
+	
+# --- Change any record tags with additional data to <record> ---
+if re.search(r'<record id=(.*?)>', filedata) is not None:
+    filedata = re.sub(r'<record id=(.*?)>', r"<record>", filedata)
+    print ('Changing <record> tags with extra data to <record>')	
+	
+# --- DATE FIXES IN THIS SECTION ---
 
     # --- Change any dc:date.created tags with additional data in timestamp to dc:date and converting to YYYY-MM-DD ---
 if re.search(r'<dc:date.created>(\d{4})-(\d{2})-(\d{2})(.*?)</dc:date.created>', filedata) is not None:
     filedata = re.sub(r'<dc:date.created>(\d{4})-(\d{2})-(\d{2})(.*?)</dc:date.created>', r"<dc:date>\1-\2-\3</dc:date>", filedata)
     print ('Changing dc:date.created to dc:date and removing extra data in timestamp')
+	
+	# --- Change any dc:date tags with additional data in timestamp to YYYY-MM-DD ---
+if re.search(r'<dc:date>(\d{4})-(\d{2})-(\d{2})(.*?)</dc:date>', filedata) is not None:
+    filedata = re.sub(r'<dc:date>(\d{4})-(\d{2})-(\d{2})(.*?)</dc:date>', r"<dc:date>\1-\2-\3</dc:date>", filedata)
+    print ('Removing extra data in timestamp to <dc:date> so that it reads YYYY-MM-DD')
+	
+# --- Change any dc:date tags with dates written out YYYY Month D to YYYY-MM-DD ---
+
+if re.search(r'<dc:date>(\d{4}) December (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) December (\d{1})</dc:date>', r"<dc:date>\1-12-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) November (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) November (\d{1})</dc:date>', r"<dc:date>\1-11-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) October (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) October (\d{1})</dc:date>', r"<dc:date>\1-10-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) September (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) September (\d{1})</dc:date>', r"<dc:date>\1-09-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) August (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) August (\d{1})</dc:date>', r"<dc:date>\1-08-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) July (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) July (\d{1})</dc:date>', r"<dc:date>\1-07-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) June (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) June (\d{1})</dc:date>', r"<dc:date>\1-06-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) May (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) May (\d{1})</dc:date>', r"<dc:date>\1-05-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) April (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) April (\d{1})</dc:date>', r"<dc:date>\1-04-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) March (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) March (\d{1})</dc:date>', r"<dc:date>\1-03-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) February (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) February (\d{1})</dc:date>', r"<dc:date>\1-02-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) January (\d{1})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) January (\d{1})</dc:date>', r"<dc:date>\1-01-0\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) D to YYYY-MM-DD')	
+	
+# --- Change any dc:date tags with dates written out YYYY Month DD to YYYY-MM-DD ---
+
+if re.search(r'<dc:date>(\d{4}) December (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) December (\d{2})</dc:date>', r"<dc:date>\1-12-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) November (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) November (\d{2})</dc:date>', r"<dc:date>\1-11-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) October (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) October (\d{2})</dc:date>', r"<dc:date>\1-10-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) September (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) September (\d{2})</dc:date>', r"<dc:date>\1-09-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) August (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) August (\d{2})</dc:date>', r"<dc:date>\1-08-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) July (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) July (\d{2})</dc:date>', r"<dc:date>\1-07-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) June (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) June (\d{2})</dc:date>', r"<dc:date>\1-06-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) May (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) May (\d{2})</dc:date>', r"<dc:date>\1-05-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) April (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) April (\d{2})</dc:date>', r"<dc:date>\1-04-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) March (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) March (\d{2})</dc:date>', r"<dc:date>\1-03-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) February (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) February (\d{2})</dc:date>', r"<dc:date>\1-02-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+if re.search(r'<dc:date>(\d{4}) January (\d{2})</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) January (\d{2})</dc:date>', r"<dc:date>\1-01-\2</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) DD to YYYY-MM-DD')
+
+# --- Change any dc:date tags with dates written out YYYY Month to YYYY-MM---
+
+if re.search(r'<dc:date>(\d{4}) December</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) December</dc:date>', r"<dc:date>\1-12</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) November</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) November</dc:date>', r"<dc:date>\1-11</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) October</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) October</dc:date>', r"<dc:date>\1-10</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) September</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) September</dc:date>', r"<dc:date>\1-09</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) August</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) August</dc:date>', r"<dc:date>\1-08</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) July</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) July</dc:date>', r"<dc:date>\1-07</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) June</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) June</dc:date>', r"<dc:date>\1-06</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) May</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) May</dc:date>', r"<dc:date>\1-05</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) April</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) April</dc:date>', r"<dc:date>\1-04</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) March</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) March</dc:date>', r"<dc:date>\1-03</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) February</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) February</dc:date>', r"<dc:date>\1-02</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')
+
+if re.search(r'<dc:date>(\d{4}) January</dc:date>', filedata) is not None:
+	filedata = re.sub(r'<dc:date>(\d{4}) January</dc:date>', r"<dc:date>\1-01</dc:date>", filedata)
+	print ('Changing dc:date dates labeled YYYY (month) to YYYY-MM')	
 
 # --- Look for semicolons at the end of string (CONTENTdm specfic)
 #if filedata.find(';<') is not None:
